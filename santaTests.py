c26c3acd846ab350365791f743a5e5353689979b
Colin Grove <grove.colin@gmail.com>
2014-12-02 19:19:38 -0600

    wrote testFunction() to abstract testing I/O

--- santaTests.py
+++ santaTests.py
@@ -1,29 +1,29 @@
 from santa import *
 
-def testFunction(function, cases):
+def testFunctionCases(function, cases):
   passed = True
+  errStr = ""
   for case in cases:
     [argList, output] = case
     result = function(*argList)
     if not(output == result):
-      print "Case " + str(case) + " failed."
-      print "[output, result]: " + str([output, result])
-    passed = passed and (output == result)
-  return passed
+      errStr = errStr + "\nCase " + str(case) + " failed."
+      errStr = errStr + "\n[output, result]: " + str([output, result])
+    passed = (passed and (output == result))
+  return passed, errStr
 
+def testFunction(function, cases):
+  funName = function.__name__
+  print("Testing " + funName + "...")
+  testResult, errStr = testFunctionCases(function, cases)
+  if testResult:
+    print("Pass.")
+  else:
+    print("Fail.\n" + errStr)
 
 if __name__ == '__main__':
 
-  print("Testing onMinToProd...")
+  testFunction(onMinToProd, onMinToProd_cases)
+  testFunction(timeLeftToday, timeLeftToday_cases)
 
-  if testFunction(onMinToProd, onMinToProd_cases):
-    print("Pass.")
-  else:
-    print("Fail.")
 
-  print("Testing timeLeftToday...")
-  
-  if testFunction(timeLeftToday, timeLeftToday_cases):
-    print("Pass.")
-  else:
-    print("Fail.")
